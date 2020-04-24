bf3f26debfe92a26d2dc24bf982e180bff28c47d
이찬호 <devho813@gmail.com>
2020-04-25 03:55:41 +0900

    signin 마크업 수정

--- src/components/signin/Login.tsx
+++ src/components/signin/Login.tsx
@@ -7,7 +7,9 @@ function Login() {
     <LoginContainer>
       <LoginWrapper>
         <MainCharacterContainer>
-          <img src="/images/test_main_character.jpg" alt="테스트 메인 캐릭터" />
+          <div>
+            <img src="/images/test_main_character.jpg" alt="테스트 메인 캐릭터" />
+          </div>
         </MainCharacterContainer>
         <h2>
           <span>Sign In</span>
@@ -30,11 +32,16 @@ const LoginWrapper = styled.div`
   margin: 0 auto;
   padding: 2.5rem;
   border-radius: 10px;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+  transition: box-shadow 0.5s;
+
+  &:hover {
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.4);
+  }
 
   h2 {
     position: relative;
-    margin: 1em 0;
+    margin: 1em 0 1.2em;
     font-size: 1.5rem;
 
     span {
@@ -45,7 +52,8 @@ const LoginWrapper = styled.div`
     a {
       position: absolute;
       right: 0;
-      top: 0;
+      top: 50%;
+      transform: translateY(-50%);
       font-size: 0.9rem;
       text-decoration: underline;
       cursor: pointer;
@@ -53,15 +61,25 @@ const LoginWrapper = styled.div`
   }
 `;
 
-const MainCharacterContainer = styled.div`
+const MainCharacterContainer = styled.header`
   width: 100%;
-  text-align: center;
 
-  img {
-    display: inline-block;
+  div {
+    margin: 0 auto;
     width: 50%;
-    height: auto;
     border-radius: 50%;
     user-select: none;
+    overflow: hidden;
+
+    img {
+      display: inline-block;
+      width: 100%;
+      height: auto;
+      transition: transform 0.5s;
+
+      :hover {
+        transform: scale(1.2) translateY(8%);
+      }
+    }
   }
 `;
