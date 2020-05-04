154bcfdce7342448417ec7e1d4403adc91a713e1
devho <devho813@gmail.com>
2020-05-04 16:38:14 +0900

    board 마크업

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
-import Link from 'next/link';
 import { withApollo } from '../graphql/apollo/apollo';
-import withAuth from '../utils/withAuth';
+import Board from '../components/board/Board';
 
 function Home() {
   return (
-    <Title>
-      <Link href="/signin">
-        <a>로그인 페이지로 고고</a>
-      </Link>
-    </Title>
+    <>
+      <Board />
+    </>
   );
 }
 
-export default withApollo()(withAuth(Home));
-
-const Title = styled.h1`
-  color: #fabdb9;
-  font-size: 50px;
-`;
+export default withApollo()(Home);
