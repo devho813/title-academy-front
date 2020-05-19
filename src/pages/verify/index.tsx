3988012e8a6277ce8771411ff0e6d5cf2b8c7992
이찬호 <devho813@gmail.com>
2020-05-20 02:05:36 +0900

    정적 페이지 query string 이슈 수정

--- src/pages/verify/index.tsx
+++ src/pages/verify/index.tsx
@@ -6,12 +6,8 @@ import ChangePassword from '../../components/verify/ChangePassword';
 import { withApollo } from '../../graphql/apollo/apollo';
 
 function Verify() {
-  const {
-    asPath,
-    query: { authCode },
-  } = useRouter();
-
-  const { email } = useVerify(authCode ? (authCode as string) : asPath.split('authCode=')[1]);
+  const { asPath } = useRouter();
+  const { email } = useVerify(asPath.split('authCode=')[1]);
 
   return (
     <AuthLayout>
